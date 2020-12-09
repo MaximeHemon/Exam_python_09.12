a1766929e53f8738c62f73679494166958b3d43b
MaximeHemon <maximehemon56@gmail.com>
2020-12-09 17:26:01 +0100

    Update Exam_python_9.12.py

--- Exam_python_9.12.py
+++ Exam_python_9.12.py
@@ -1,7 +1,3 @@
-from colorama import init 
-init()
-from colorama import Fore, Back, Style 
-
 mot1={"c","a","s","t","o","r"}
 mot2={"c","i","n","e","m","a"}
 mot3={"c","i","t","r","o","n"}
@@ -18,7 +14,7 @@ def motafficher (mot1,mot2,mot3,mot4,mot5,mot6,mot7,mot8,mot9,mot10):
 	from random import shuffle
 	motafficher= [[i] for i in range(10)]
 	shuffle(motafficher)
-print (motafficher)
+print (len(motafficher))
   #  for i in range (0,len(motafficher)):
   #  print random.shuffle(motafficher)
 input()
