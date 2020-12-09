a91959aae4ae2e219665d5d2aac9409434030b8a
MaximeHemon <maximehemon56@gmail.com>
2020-12-09 16:59:04 +0100

    Update Exam_python_9.12.py

--- Exam_python_9.12.py
+++ Exam_python_9.12.py
@@ -5,16 +5,20 @@ from colorama import Fore, Back, Style
 mot1={"c","a","s","t","o","r"}
 mot2={"c","i","n","e","m","a"}
 mot3={"c","i","t","r","o","n"}
-mot4={"l","e","t","t","r","e"}
-mot5={"i","n","d","i","c","e"}
+mot4={"h","u","m","a","i","n"}
+mot5={"a","s","p","e","c","t"}
 mot6={"r","e","l","i","e","f"}
 mot7={"v","a","l","e","u","r"}
 mot8={"n","a","t","u","r","e"}
 mot9={"h","u","m","e","u","r"}
-mot10={"a","l","l","u","r","e"}
+mot10={"m","a","r","q","u","e"}
 
 import random
-def motafficher (mot1,mot2,mot3,mot4,mot5,mot6):
-    print random.randint(0,9)
-    
+def motafficher (mot1,mot2,mot3,mot4,mot5,mot6,mot7,mot8,mot9,mot10):
+	from random import shuffle
+	motafficher= [[i] for i in range(10)]
+	shuffle(motafficher)
+print (motafficher)
+  #  for i in range (0,len(motafficher)):
+  #  print random.shuffle(motafficher)
 input()
