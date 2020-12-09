f7e155bc0de2602793b43421c2cbe71186529ad7
MaximeHemon <maximehemon56@gmail.com>
2020-12-09 15:35:52 +0100

    Update Exam_python_9.12.py

--- Exam_python_9.12.py
+++ Exam_python_9.12.py
@@ -1,8 +1,6 @@
-import random
 from colorama import init 
 init()
 from colorama import Fore, Back, Style 
-
 
 mot1={"c","a","s","t","o","r"}
 mot2={"c","i","n","e","m","a"}
@@ -10,14 +8,13 @@ mot3={"c","i","t","r","o","n"}
 mot4={"l","e","t","t","r","e"}
 mot5={"i","n","d","i","c","e"}
 mot6={"r","e","l","i","e","f"}
+mot7={"v","a","l","e","u","r"}
+mot8={"n","a","t","u","r","e"}
+mot9={"h","u","m","e","u","r"}
+mot10={"a","l","l","u","r","e"}
 
-def motrandom (mot1,mot2,mot3,mot4,mot5,mot6)
-
-
-
-print(Fore.RED + 'some red text', end=" ") 
-print(Back.GREEN + 'and with a green background') 
-print(Style.DIM + 'and in dim text') 
-print(Style.RESET_ALL) 
-print('back to normal now') 
+import random
+def motafficher (mot1,mot2,mot3,mot4,mot5,mot6):
+    print random.randint(0,9)
+    
 input()
